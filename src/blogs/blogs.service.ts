7c1145675f012e2687abbd74f107032c9658480d
rohansolse <rohansolse@gmail.com>
2022-05-04 16:30:33 +0530

    providers done

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BlogsService {}
+export class BlogsService {
+    private readonly blogs: any[] = [];
+
+    findData(): any[] {
+        return this.blogs;
+    }
+
+    create(blog: any): any {
+        this.blogs.push(blog);
+        return this.blogs;
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get, Param, HttpCode, Post, Body } from '@nestjs/common';
+import { BlogsService } from '../blogs/blogs.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(private readonly blogsService: BlogsService) { }
+
     @Get()
     userInfo(): string {
         return 'This action returns all users';
@@ -23,5 +27,16 @@ export class UsersController {
     @HttpCode(200)
     userList(@Param('id') id: string): string {
         return `This action returns a #${id} user`;
+    }
+
+    @Get('blog-list')
+    bloglist(): any {
+        return this.blogsService.findData();
+    }
+
+    @Post('blog-add')
+    blogAdd(@Body() record: any): any {
+        console.log(record, 'record');
+        return this.blogsService.create(record);
     }
 }
