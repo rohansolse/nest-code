ad8deeb3a55b0e159746c9aff001cbe5bf1ea57b
rohansolse <rohansolse@gmail.com>
2022-05-04 13:48:57 +0530

    service created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, HttpCode, Post, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +10,18 @@ export class UsersController {
     @Get('history')
     userHistory(): string {
         return 'This action returns all Old users';
+    }
+
+    @Post('list')
+    @HttpCode(200)
+    user(@Body() user: string): string {
+        console.log(user);
+        return `This action returns a user`;
+    }
+
+    @Get('list/:id')
+    @HttpCode(200)
+    userList(@Param('id') id: string): string {
+        return `This action returns a #${id} user`;
     }
 }
